0542939325183f707d402c0609f8a206abfe6b94
chaeeeeyeon <132416108+chaeeeeyeon@users.noreply.github.com>
2023-07-23 16:02:05 +0900

    Add files via upload

---
+++ script/slide.js
@@ -0,0 +1,24 @@
+$(function () {
+
+  $('.slidemap').slick({
+    dots: true,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 2,
+    slidesToScroll: 2,
+    responsive: [
+      {
+        breakpoint: 950,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object  });
+      }
+    ]
+});
+});
